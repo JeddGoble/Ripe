3bc906a325116fdf7fd1338f97e9f118eb64d84d
Jedd Goble <jeddgoble@gmail.com>
2018-09-18 13:48:40 -0700

    Initial Commit

---
+++ Ripe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ripe
+//
+//  Created by Jedd Goble on 9/18/18.
+//  Copyright © 2018 Jedd Goble. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
